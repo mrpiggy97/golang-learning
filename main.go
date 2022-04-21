ef9e757954b79f660df222b94fac53c7230bd9bf
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-04-21 15:22:05 +0000

    added functions that apply mutexes

--- main.go
+++ main.go
@@ -1,41 +1,21 @@
 package main
 
 import (
-	"encoding/json"
-	"io"
-	"net/http"
+	"fmt"
+	"sync"
 
-	"github.com/mrpiggy97/golang-learning/project"
-	"github.com/rs/zerolog/log"
+	"github.com/mrpiggy97/golang-learning/mutexes"
 )
 
 // Main will apply all concepts learned.
 
-func RequestHandler(writer http.ResponseWriter, request *http.Request, jobQueue chan project.Job) {
-	if request.Method != "POST" {
-		writer.WriteHeader(http.StatusMethodNotAllowed)
-	}
-
-	jsonData, _ := io.ReadAll(request.Body)
-	var jobData *project.Job = new(project.Job)
-	var unMarshalingError error = json.Unmarshal(jsonData, jobData)
-	if unMarshalingError != nil {
-		log.Error().Msg(unMarshalingError.Error())
-	}
-	jobQueue <- *jobData
-}
-
 func main() {
-	const (
-		maxWorkers   = 4
-		maxQueueSize = 20
-		port         = "0.0.0.0:8081"
-	)
-	var jobQueue chan project.Job = make(chan project.Job, maxQueueSize)
-	var dispatcher *project.Dispatcher = project.NewDispatcher(jobQueue, maxWorkers)
-	dispatcher.Run()
-	http.HandleFunc("/fib", func(writer http.ResponseWriter, req *http.Request) {
-		RequestHandler(writer, req, jobQueue)
-	})
-	http.ListenAndServe(port, nil)
+	var waiter *sync.WaitGroup = new(sync.WaitGroup)
+	var mutexer *sync.RWMutex = new(sync.RWMutex)
+	for i := 1; i <= 5; i++ {
+		waiter.Add(1)
+		go mutexes.Deposit(waiter, i*100, mutexer)
+	}
+	waiter.Wait()
+	fmt.Println(mutexes.Balance(mutexer))
 }
